a93aae2d4b295b801ad1f94810efe1619dc4949c
Kishan Trivedi <trivedikishan12345@gmail.com>
2025-07-13 19:06:44 -0400

    Interval List Intersections

---
+++ Interval List Intersections.py
@@ -0,0 +1,26 @@
+class Solution:
+    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        # Check for edge cases
+        if not secondList or not firstList:
+            return []
+
+        output = []
+
+        p1 = 0
+        p2 = 0
+
+        while p1 < len(firstList) and p2 < len(secondList):
+            s1 = firstList[p1][0]
+            e1 = firstList[p1][1]
+
+            s2 = secondList[p2][0]
+            e2 = secondList[p2][1]
+
+            # Check if overlap exist
+            if s1 <= e2 and s2 <= e1:
+                output.append([max(s1,s2), min(e1,e2)])
+                
+            if e1 <= e2:
+                p1 += 1
+            else:
+                p2 += 1
