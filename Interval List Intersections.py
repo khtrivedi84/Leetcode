9560d339076e0e921768b027759d3f3f917d35dd
Kishan Trivedi <trivedikishan12345@gmail.com>
2025-07-13 19:07:49 -0400

    Added brute force to Interval List Intersections

--- Interval List Intersections.py
+++ Interval List Intersections.py
@@ -1,5 +1,15 @@
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        # Brute force
+        output = []
+        for a in firstList:
+            for b in secondList:
+                start = max(a[0], b[0])
+                end = min(a[1], b[1])
+                if start <= end:  # This confirms there is an overlap
+                    output.append([start, end])
+        return output
+    
         # Check for edge cases
         if not secondList or not firstList:
             return []
@@ -19,8 +29,9 @@ class Solution:
             # Check if overlap exist
             if s1 <= e2 and s2 <= e1:
                 output.append([max(s1,s2), min(e1,e2)])
-                
+
             if e1 <= e2:
                 p1 += 1
             else:
                 p2 += 1
+        return output
